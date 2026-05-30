77737149428ede7b75e511bca2375b676d55dc86
张雨松 <zhangyusong@zhangyusongdeMacBook-Pro.local>
2026-05-30 14:27:19 +0800

    fix: 分离数据库和 Edge Functions 的密钥

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
   SUPABASE_URL: 'https://whiiugqnyhrnwsvmtnif.supabase.co',
-  SUPABASE_KEY: 'sb_secret_7PiElp4bM8fFVOCtQyRaJA_t1O21C5m'
+  SUPABASE_KEY: 'sb_publishable_6ubZtWTSd8RmTs3vzqy_6w_xOkoxUw2',
+  SUPABASE_SECRET_KEY: 'sb_secret_7PiElp4bM8fFVOCtQyRaJA_t1O21C5m'
 };
